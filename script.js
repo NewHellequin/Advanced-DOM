6fb948394c8124fc87b73832ab7b63096614dec0
NewHellequin <128534288+NewHellequin@users.noreply.github.com>
2024-04-08 14:58:50 +0000

    More ways to interact with elements

--- script.js
+++ script.js
@@ -34,44 +34,96 @@ document.addEventListener('keydown', function (e) {
 ////////////////////////////////
 ////////////////////////////////
 
-// console.log(document.documentElement);
-// console.log(document.head);
-// console.log(document.body);
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
 
-// const header = document.querySelector('.header');
-// const allSections = document.querySelectorAll('.section');
-// console.log(allSections);
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
 
-// document.getElementById('section--1');
-// const allButtons = document.getElementsByTagName('button');
-// console.log(allButtons);
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
 
-// console.log(document.getElementsByClassName('btn'));
+console.log(document.getElementsByClassName('btn'));
 
-// // Creating and inserting elements
-// // .insertAdjacentHTML
+// Creating and inserting elements
+// .insertAdjacentHTML
 
-// const message = document.createElement('div');
-// message.classList.add('cookie-message');
-// message.textContent =
-//   'We use cookies for improved functionality and analytics.';
-// message.innerHTML =
-//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent =
+  'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-// header.prepend(message);
-// // header.append(message);
-// // header.append(message.cloneNode(true));
+header.prepend(message);
+// header.append(message);
+// header.append(message.cloneNode(true));
 
-// // header.before(message);
-// // header.after(message);
+// header.before(message);
+// header.after(message);
 
-// // Delete elements
-// document
-//   .querySelector('.btn--close-cookie')
-//   .addEventListener('click', function () {
-//     message.remove();
-//   });
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
 
 ////////////////////////////////
 ////////////////////////////////
 ////////////////////////////////
+
+// Styles
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+
+// Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt); // only works on standard properties
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard attribute
+console.log(logo.designer); // doesnt work
+console.log(logo.getAttribute('designer')); // works
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.getAttribute('src')); // just src title, not absolute link
+
+const link = document.querySelector('.twitter-link');
+console.log(link.href);
+
+// Data attributes
+
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// Don't use
+logo.className = 'Jonas';
