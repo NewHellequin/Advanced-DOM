3c572b37994317ffbae81b58ff8bc16e0ccf9de5
NewHellequin <128534288+NewHellequin@users.noreply.github.com>
2024-04-10 12:13:24 +0000

    Sticky Navigation

--- script.js
+++ script.js
@@ -133,6 +133,18 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1.0));
 
+// Sticky Navigation
+
+const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
+
+window.addEventListener('scroll', function () {
+  // console.log(window.scrollY);
+
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+}); // not efficient as it fires all the time
+
 ////////////////////////////////
 ////////////////////////////////
 ////////////////////////////////
