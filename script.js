c3501e409327b336a6bf2f28886be2cabe12c20b
NewHellequin <128534288+NewHellequin@users.noreply.github.com>
2024-04-10 13:09:26 +0000

    Intersection Observer API

--- script.js
+++ script.js
@@ -133,17 +133,53 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1.0));
 
-// Sticky Navigation
+///////////////////////////////
 
-const initialCoords = section1.getBoundingClientRect();
-// console.log(initialCoords);
+// // Sticky Navigation
 
-window.addEventListener('scroll', function () {
-  // console.log(window.scrollY);
+// const initialCoords = section1.getBoundingClientRect();
+// // console.log(initialCoords);
 
-  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+// window.addEventListener('scroll', function () {
+//   // console.log(window.scrollY);
+
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// }); // not efficient as it fires all the time
+
+// Sticky navigation: Intersection Observer API
+
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
-}); // not efficient as it fires all the time
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
 
 ////////////////////////////////
 ////////////////////////////////
